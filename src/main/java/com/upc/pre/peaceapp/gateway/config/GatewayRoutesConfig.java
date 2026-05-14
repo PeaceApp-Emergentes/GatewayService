d4c30a83f746424514060dacfe8010ed49d97831
TolyNoriega5 <125397518+TolyNoriega5@users.noreply.github.com>
2026-05-13 20:34:02 -0500

    feat: add initial service implementation

---
+++ src/main/java/com/upc/pre/peaceapp/gateway/config/GatewayRoutesConfig.java
@@ -0,0 +1,28 @@
+package com.upc.pre.peaceapp.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+public class GatewayRoutesConfig {
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOriginPatterns(List.of("http://localhost:5173","https://peaceapp-web.netlify.app"));
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
+    }
+}
---
+++ src/test/java/com/upc/pre/peaceapp/gateway/GatewayServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.upc.pre.peaceapp.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+
+class GatewayServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
